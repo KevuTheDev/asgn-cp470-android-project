61823c2c3495e2a544daa35fb77f68d89378494b
danwardvs <danwardvs@adsgames.net>
2022-10-31 21:42:54 -0400

    map in fragment

--- app/src/main/java/com/example/a470group/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/a470group/ui/home/HomeFragment.java
@@ -1,37 +1,61 @@
 package com.example.a470group.ui.home;
 
+
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.example.a470group.databinding.FragmentHomeBinding;
+import com.example.a470group.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class HomeFragment extends Fragment {
 
-  private FragmentHomeBinding binding;
-
-  public View onCreateView(@NonNull LayoutInflater inflater,
-                           ViewGroup container, Bundle savedInstanceState) {
-    HomeViewModel homeViewModel =
-        new ViewModelProvider(this).get(HomeViewModel.class);
-
-    binding = FragmentHomeBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
-
-    final TextView textView = binding.textHome;
-    homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-    return root;
-  }
-
   @Override
-  public void onDestroyView() {
-    super.onDestroyView();
-    binding = null;
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    // Initialize view
+    View view=inflater.inflate(R.layout.fragment_first, container, false);
+
+    // Initialize map fragment
+    SupportMapFragment supportMapFragment=(SupportMapFragment)
+        getChildFragmentManager().findFragmentById(R.id.google_map);
+
+    // Async map
+    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+      @Override
+      public void onMapReady(GoogleMap googleMap) {
+        // When map is loaded
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+          @Override
+          public void onMapClick(LatLng latLng) {
+            // When clicked on map
+            // Initialize marker options
+            MarkerOptions markerOptions=new MarkerOptions();
+            // Set position of marker
+            markerOptions.position(latLng);
+            // Set title of marker
+            markerOptions.title(latLng.latitude+" : "+latLng.longitude);
+            // Remove all marker
+            googleMap.clear();
+            // Animating to zoom the marker
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+            // Add marker on map
+            googleMap.addMarker(markerOptions);
+          }
+        });
+      }
+    });
+    // Return view
+    return view;
   }
 }
