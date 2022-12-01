08d5e39b559f3fd72bfcf51d2b7849bcc61ff678
Kevin He <kevin66he@hotmail.com>
2022-12-01 01:18:12 -0500

    Cleanup Files
    - cleaned up files that are not in use of application
    - removed unused Fragments, layouts and classes

--- app/src/main/java/com/example/a470group/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/a470group/ui/home/HomeFragment.java
@@ -30,7 +30,7 @@ public class HomeFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     // Initialize view
-    View view=inflater.inflate(R.layout.fragment_first, container, false);
+    View view=inflater.inflate(R.layout.fragment_home, container, false);
 
     // Initialize map fragment
     SupportMapFragment supportMapFragment=(SupportMapFragment)
